ca68a67efa4bf407472917973b9ba0eca34eae96
= <vemund.hatlen@gmail.com>
2022-11-06 19:48:55 +0100

    added html classes and sizing capabilities

--- terminal/wireframerenderer.js
+++ terminal/wireframerenderer.js
@@ -1,9 +1,7 @@
 let flength = 300;
-let canvas = document.getElementById("mainCanv");
-let ctx = canvas.getContext("2d");
+let renderCanvas = document.getElementsByClassName("logorender");
 let frames = 0;
 let deg = 0;
-ctx.fillStyle = "#ffffff";
 
 let fontsize = 30;
 
@@ -37,13 +35,13 @@ let lines = [
 
 let renderedVertexes = [];
 
-function projectvertex (vertex) {
+function projectvertex (vertex, canvas) {
     let x = vertex[0];
     let y = vertex[1];
     let z = vertex[2];
 
-    let xProjected = Math.floor((flength * x) / (z + flength));
-    let yProjected = Math.floor((flength * y) / (z + flength));
+    let xProjected = (flength * x) / (z + flength) * (canvas.clientWidth / 500);
+    let yProjected = (flength * y) / (z + flength) * (canvas.clientHeight / 500);
 
     return [xProjected+canvas.clientWidth/2, yProjected+canvas.clientHeight/2];
 }
@@ -81,7 +79,7 @@ function rotatevertex(vertex, degrees, axis) {
     return (vertex)
 }
 
-function drawLine(x1, y1, x2, y2) {
+function drawLine(x1, y1, x2, y2, ctx) {
     ctx.beginPath();
     ctx.moveTo(x1,y1);
     ctx.lineTo(x2,y2);
@@ -90,23 +88,29 @@ function drawLine(x1, y1, x2, y2) {
 
 // Gender time
 setInterval(() => {
-    ctx.clearRect(0,0,500,500)
-    projectedWF = [];
-    wireframe.forEach(element => {
-        let vertex = projectvertex(rotatevertex(element, frames/100, 2));
-        vertex[1] -= 50
-        projectedWF.push(vertex);
-        ctx.fillRect(vertex[0]-1, vertex[1]-1, 3, 2);
-    });
-    lines.forEach(line => {
+    for (let i = 0; i < renderCanvas.length; i++) {
+        let canvas = renderCanvas[i];
+        let ctx = canvas.getContext("2d");
+        ctx.fillStyle = "#ffffff"
+        ctx.clearRect(0,0,500,500)
+        projectedWF = [];
+        wireframe.forEach(element => {
+            let vertex = projectvertex(rotatevertex(element, frames/100, 2), canvas);
+            vertex[1] -= canvas.clientHeight/10
+            projectedWF.push(vertex);
+            ctx.fillRect(vertex[0]-1, vertex[1]-1, 3, 2);
+        });
+        lines.forEach(line => {
 
-        let vertex1 = projectedWF[line[0]]
-        let vertex2 = projectedWF[line[1]]
-        drawLine(vertex1[0], vertex1[1], vertex2[0], vertex2[1]);
-    });
-    fontsize = 50 + Math.sin(frames/50)*5 - 10;
-    ctx.font = fontsize+"px Consolas";
-    let textwidth = ctx.measureText("[ Averieyy ]");
-    ctx.fillText("[ Averieyy ]", 250-(textwidth.width)/2, 325, 500);
-    frames+=1;
+            let vertex1 = projectedWF[line[0]]
+            let vertex2 = projectedWF[line[1]]
+            drawLine(vertex1[0], vertex1[1], vertex2[0], vertex2[1], ctx);
+        });
+        fontsize = canvas.width/10 + Math.sin(frames/50)*(canvas.width/100) - canvas.width/50;
+        ctx.font = fontsize+"px Consolas";
+        let textwidth = ctx.measureText("[ Averieyy ]");
+        ctx.fillText("[ Averieyy ]", canvas.clientWidth/2-(textwidth.width)/2, canvas.clientHeight*3/4, 500);
+        frames+=1;
+    };
+    
 }, 1000/60);
